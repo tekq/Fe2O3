98d7250c69f37886dba3f93ecb77d95fe0e8bd65
tekq <81379378+tekq@users.noreply.github.com>
2022-06-19 14:25:14 +0300

    Better prompt and bug fixes
    also unfinished features that are probs still in the code, will maybe add them next time i update

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use std::process::{Command, exit};
 use std::env;
 use std::path::Path;
 use itertools::{concat, Itertools};
-use url::Url;
+use std::fmt;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     let clone_args: Vec<String> = env::args().collect();
 
+    let a = Command::new("xbps-query")
+            .arg("-Rs")
+            .arg("neofetch")
+            .status()
+            .expect("Didn't work.");
+
+    println!("{:?}", a);
 
     if args.len() >= 2 { // detect action
             let action = &clone_args[1];
@@ -36,13 +43,22 @@ fn main() {
                 let mut package_to_install = 0;
 
                 while package_to_install < args.len() {
-                    println!("Installing package {0}/{1}", package_to_install + 1, args.len());
+                    println!("{0}ing package {1}/{2}", action, package_to_install + 1, args.len());
                     // println!("{}", "test".to_owned() + &args[package_to_install]);
                     let mut path = "/etc/elements/repos/Nitrogen/".to_owned() + &args[package_to_install];
 
                     if Path::new(&path).exists() {
-                        println!("Wow! You exist!");
-                        let exec = path.to_owned() + "/build";
+                        let exec = ""; // declare exec so compiler doesn't scream
+
+                        if action.eq("install"){
+                            let exec = path.to_owned() + "/build";
+                        } else if action.eq("remove") {
+                            let exec = path.to_owned() + "/remove";
+                        } else if action.eq("update") {
+                            let exec = path.to_owned() + "/build";
+                        }
+
+
                         Command::new("bash")
                             .arg(&exec)
                             .output()
@@ -54,13 +70,6 @@ fn main() {
                         println!("{0} is not in any repository.", args[package_to_install]);
                     }
 
-                    // if Path::new(&path).exists() {
-                    //     Command::new("bash")
-                    //         .arg(exec)
-                    //         .output()
-                    //         .expect("Im outta here!");
-                    // }
-
                     package_to_install = package_to_install + 1;
                     if package_to_install == args.len() {
                         exit(0);
@@ -71,12 +80,5 @@ fn main() {
                 println!("No package specified to {0}.", action);
                 exit(2);
             }
-
-
         }
-
-        // Command::new("bash")
-        //     .arg("/home/vlad/code/test")
-        //     .output()
-        //     .expect("Install command failed to start");
 }
