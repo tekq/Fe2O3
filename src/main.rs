17b19791faa65ee54e23fafa8a3326fd2e07d462
tekq <81379378+tekq@users.noreply.github.com>
2022-06-26 00:57:06 +0300

    Updated Nix 0.15.0 -> 0.24.1, Bugfixes and Help Message
    This is most probably going to be a new release, it has:
    *an updated version of the library Nix
    * a repaired updater, will now update the system again
    * Elements updater, update will also update Elements itself from the latest GitHub release
    * added a new shiny Help message whenever the first argument is missing or by using 'fe2o3 help'

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,14 @@ fn main() {
             // pass
         } else if action.to_lowercase().eq("search") {
             // pass
+        } else if action.to_lowercase().eq("help") {
+            println!("usage: lmt <action> <package>");
+            println!("List of Main Commands:");
+            println!("  install: Install a package");
+            println!("  remove: Remove a package");
+            println!("  update: Update all packages");
+            println!("  help: Show this help message");
+            exit(0);
         } else {
             println!("No argument called '{0}' found.", action);
             exit(1);
@@ -69,6 +77,11 @@ fn main() {
             let mut package_to_install = 0;
 
             while package_to_install < args.len() {
+                if ["elements", "gnome-core", "gnome", "linux", "xbps"].contains(&&*args[package_to_install]) {
+                    println!("Cannot remove {}: Package is required by the system.", &args[package_to_install]);
+                    exit(256);
+                }
+
                 let mut pkg_db_path = File::open("/etc/elements/.pkg.db").unwrap();
                 let mut updated_pkg_db = String::new();
                 pkg_db_path.read_to_string(&mut updated_pkg_db).unwrap();
@@ -96,23 +109,23 @@ fn main() {
                             let updated_pkg_db = updated_pkg_db.replace(&args[package_to_install], "");
                             write_to_package_db(updated_pkg_db);
                         }
-
-                        println!("Removing package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
-                        Command::new("bash")
-                            .arg(path.to_owned() + "/remove")
-                            .output()
-                            .expect("Didn't work.");
-                    } else if action.to_string().eq("update") {
-                        if !updated_pkg_db.contains(&args[package_to_install]) {
-                            println!("Cannot update {}: Package not installed.", &args[package_to_install]);
-                            exit(256);
-                        }
-                        println!("Updating package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
-                        Command::new("bash")
-                            .arg(path.to_owned() + "/build")
-                            .output()
-                            .expect("Didn't work.");
                     }
+
+                    println!("Removing package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
+                    Command::new("bash")
+                        .arg(path.to_owned() + "/remove")
+                        .output()
+                        .expect("Didn't work.");
+                } else if action.to_string().eq("update") {
+                    if !updated_pkg_db.contains(&args[package_to_install]) {
+                        println!("Cannot update {}: Package not installed.", &args[package_to_install]);
+                        exit(256);
+                    }
+                    println!("Updating package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
+                    Command::new("bash")
+                        .arg(path.to_owned() + "/build")
+                        .output()
+                        .expect("Didn't work.");
                 } else {
                     if action.eq("install") {
                         Command::new("xbps-install")
@@ -145,9 +158,62 @@ fn main() {
                 }
             }
         } else {
-            println!("No package specified to {0}.", action.to_lowercase());
-            exit(2);
+            if action.to_lowercase().eq("update") {
+                println!("Updating Void packages 1/4");
+                Command::new("xbps-install")
+                    .arg("-Suy")
+                    .output()
+                    .expect("Couldn't execute xbps");
+
+                println!("Removing old repository 2/4");
+                Command::new("rm")
+                    .arg("-rf")// forced recursively remove
+                    .arg("/etc/elements/repos/Nitrogen")// path to remove
+                    .output()
+                    .expect("Couldn't remove repository.");
+
+                println!("Reclone Repository 3/4");
+                Command::new("git")
+                    .arg("clone")
+                    .arg("https://github.com/NitrogenLinux/elements-repo.git")// Nitrogen Linux's main repository
+                    .arg("/etc/elements/repos/Nitrogen")// path to clone to
+                    .output()
+                    .expect("Couldn't clone the repository.");
+                println!("Reinstall elements 4/4");
+                Command::new("curl")
+                    .arg("-s")
+                    .arg("https://api.github.com/repos/NitrogenLinux/Elements/releases/latest")// get the latest release
+                    .arg("| grep 'browser_download_url.*lmt'")// get the download link
+                    .arg("| cut -d : -f 2,3 ")// get the download link
+                    .arg("| tr -d \"")
+                    .arg("wget -qi -")// download the file
+                    .output()
+                    .expect("Couldn't execute curl");
+                Command::new("mv")
+                    .arg("lmt")
+                    .arg("/usr/bin/lmt") // move the file to /usr/bin/lmt
+                    .output()
+                    .expect("Couldn't move the file.");
+                Command::new("chmod")
+                    .arg("+x")
+                    .arg("/usr/bin/lmt") // make the file executable
+                    .output()
+                    .expect("Couldn't make the file executable.");
+                println!("Update complete. A restart may be needed to use new libraries and/or kernels.");
+                exit(0);
+            } else {
+                println!("No package specified to {0}.", action.to_lowercase());
+                exit(2);
+            }
         }
+    } else {
+        println!("usage: lmt <action> <package>");
+        println!("List of Main Commands:");
+        println!("  install: Install a package");
+        println!("  remove: Remove a package");
+        println!("  update: Update all packages");
+        println!("  help: Show this help message");
+        exit(1);
     }
 }
 
@@ -156,7 +222,7 @@ fn write_to_package_db(package: String) -> std::io::Result<()> {
     let mut package_db = File::create("/etc/elements/.pkg.db").unwrap();
     package_db.write_all(package.as_bytes()).expect("write failed");
 
-    let mut input = File::open("/tmp/temp")?;
+    let mut input = File::open("/etc/elements/.pkg.db")?;
     let mut input_buffer = String::new();
     input.read_to_string(&mut input_buffer)?;
     Ok(())
