593c859cd6c836ee888b7ac274ec89a6edb0ba02
tekq <81379378+tekq@users.noreply.github.com>
2022-06-26 01:39:22 +0300

    Add logging for every installation/removal/update
    For easier debugging, and also easier support, I added a lot for most commands, logs will be in several files like "/tmp/install.log" "/tmp/remove.log" or "/tmp/update.log"

--- src/main.rs
+++ src/main.rs
@@ -97,10 +97,13 @@ fn main() {
                             write_to_package_db(updated_pkg_db);
                         }
 
-                        Command::new("bash")
+                        let mut build_log = Command::new("bash")
                             .arg(path.to_owned() + "/build")
                             .output()
                             .expect("Didn't work.");
+
+                        let mut build_log_file = File::create("/tmp/build.log").unwrap();
+                        build_log_file.write_all(&build_log.stdout).unwrap();
                     } else if action.to_string().eq("remove") {
                         if !updated_pkg_db.contains(&args[package_to_install]) {
                             println!("Cannot remove {}: Package not installed.", &args[package_to_install]);
@@ -109,42 +112,55 @@ fn main() {
                             let updated_pkg_db = updated_pkg_db.replace(&args[package_to_install], "");
                             write_to_package_db(updated_pkg_db);
                         }
+                        println!("Removing package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
+                        let remove_log = Command::new("bash")
+                            .arg(path.to_owned() + "/remove")
+                            .output()
+                            .expect("Didn't work.");
+                        let mut remove_log_file = File::create("/tmp/remove.log").unwrap();
+                        remove_log_file.write_all(&remove_log.stdout).unwrap();
                     }
-
-                    println!("Removing package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
-                    Command::new("bash")
-                        .arg(path.to_owned() + "/remove")
-                        .output()
-                        .expect("Didn't work.");
                 } else if action.to_string().eq("update") {
                     if !updated_pkg_db.contains(&args[package_to_install]) {
                         println!("Cannot update {}: Package not installed.", &args[package_to_install]);
                         exit(256);
                     }
                     println!("Updating package {0} {1}/{2}", &args[package_to_install], package_to_install + 1, args.len()); // print action and the number of packages remaining
-                    Command::new("bash")
+                    let mut update_Log = Command::new("bash")
                         .arg(path.to_owned() + "/build")
                         .output()
                         .expect("Didn't work.");
+
+                    let mut update_log_file = File::create("/tmp/update.log").unwrap();
+                    update_log_file.write_all(&update_Log.stdout).unwrap();
                 } else {
                     if action.eq("install") {
-                        Command::new("xbps-install")
+                        let build_log = Command::new("xbps-install")
                             .arg("-Sy")
                             .arg(&args[package_to_install])
                             .output()
                             .expect("Couldn't execute xbps");
+
+                        let mut build_log_file = File::create("/tmp/build.log").unwrap();
+                        build_log_file.write_all(&build_log.stdout).unwrap();
                     } else if action.eq("remove") {
-                        Command::new("xbps-remove")
+                        let removal_log = Command::new("xbps-remove")
                             .arg("-y")
                             .arg(&args[package_to_install])
                             .output()
                             .expect("Couldn't execute xbps");
+
+                        let mut removal_log_file = File::create("/tmp/build.log").unwrap();
+                        removal_log_file.write_all(&removal_log.stdout).unwrap();
                     } else if action.eq("update") {
-                        Command::new("xbps-install")
+                        let mut update_log = Command::new("xbps-install")
                             .arg("-Sy")
                             .arg(&args[package_to_install])
                             .output()
                             .expect("Couldn't execute xbps");
+
+                        let mut update_log_file = File::create("/tmp/build.log").unwrap();
+                        update_log_file.write_all(&update_log.stdout).unwrap();
                     } else {
                         println!("{} is not a valid action.", action);
                         exit(1);
@@ -159,46 +175,53 @@ fn main() {
             }
         } else {
             if action.to_lowercase().eq("update") {
-                println!("Updating Void packages 1/4");
-                Command::new("xbps-install")
-                    .arg("-Suy")
-                    .output()
-                    .expect("Couldn't execute xbps");
+                // println!("Updating Void packages 1/4");
+                // let p1_log = Command::new("xbps-install")
+                //     .arg("-Suy")
+                //     .output()
+                //     .expect("Couldn't execute xbps");
 
                 println!("Removing old repository 2/4");
-                Command::new("rm")
+                let p2_log = Command::new("rm")
                     .arg("-rf")// forced recursively remove
                     .arg("/etc/elements/repos/Nitrogen")// path to remove
                     .output()
                     .expect("Couldn't remove repository.");
 
                 println!("Reclone Repository 3/4");
-                Command::new("git")
+                let p3_log = Command::new("git")
                     .arg("clone")
                     .arg("https://github.com/NitrogenLinux/elements-repo.git")// Nitrogen Linux's main repository
                     .arg("/etc/elements/repos/Nitrogen")// path to clone to
                     .output()
                     .expect("Couldn't clone the repository.");
                 println!("Reinstall elements 4/4");
-                Command::new("curl")
+                let p4_log = Command::new("curl")
                     .arg("-s")
-                    .arg("https://api.github.com/repos/NitrogenLinux/Elements/releases/latest")// get the latest release
-                    .arg("| grep 'browser_download_url.*lmt'")// get the download link
-                    .arg("| cut -d : -f 2,3 ")// get the download link
-                    .arg("| tr -d \"")
-                    .arg("wget -qi -")// download the file
+                    .arg("https://api.github.com/repos/NitrogenLinux/Elements/releases/latest | grep 'browser_download_url.*lmt' | cut -d : -f 2,3 | tr -d \" | wget -qi -")// get the latest release
                     .output()
                     .expect("Couldn't execute curl");
-                Command::new("mv")
+                let mv_log = Command::new("mv")
+                    .arg("-v")// verbose for logging
                     .arg("lmt")
                     .arg("/usr/bin/lmt") // move the file to /usr/bin/lmt
                     .output()
                     .expect("Couldn't move the file.");
-                Command::new("chmod")
+                let chmod_log = Command::new("chmod")
                     .arg("+x")
+                    .arg("-v") // verbose for logging
                     .arg("/usr/bin/lmt") // make the file executable
                     .output()
                     .expect("Couldn't make the file executable.");
+
+                let mut update_log_file = File::create("/tmp/update.log").unwrap();
+                // update_log_file.write_all(&p1_log.stdout).unwrap();
+                update_log_file.write_all(&p2_log.stdout).unwrap();
+                update_log_file.write_all(&p3_log.stdout).unwrap();
+                update_log_file.write_all(&p4_log.stdout).unwrap();
+                update_log_file.write_all(&mv_log.stdout).unwrap();
+                update_log_file.write_all(&chmod_log.stdout).unwrap();
+
                 println!("Update complete. A restart may be needed to use new libraries and/or kernels.");
                 exit(0);
             } else {
